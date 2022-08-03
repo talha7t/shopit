5980ce52c7b4ec3c1b1a9a4908e11edebb927825
talha7t <48598366+talha7t@users.noreply.github.com>
2022-08-03 18:16:51 +0500

    going to deplpoy

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,7 +6,7 @@ import aboutStyles from "../styles/about.module.css";
 const About = () => {
   return (
     <div class={aboutStyles.section}>
-      <div class={aboutStyles.container}>
+      <div class={aboutStyles.container + " p-5"}>
         <div class={aboutStyles.content_section}>
           <div class={aboutStyles.title}>
             <h1>About Us</h1>
@@ -14,18 +14,21 @@ const About = () => {
           <div class={aboutStyles.content}>
             <h3>Welcome to SHOP IT</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              We draw our inspirations from the diverse global arena and pack it
+              under one roof, to share the fact that beauty is universal. We
+              constantly strive to challenge the limitations of all the
+              possibilities when it comes to weaving, printing, embroidery &amp;
+              embellishments. The cloth used is not mere fabric for us, we treat
+              it like an artist treats his canvas; with the love, respect and
+              dedication that it deserves. Here at SHOP IT, we understand that
+              style and fashion are a vital part of your lives and so we strive
+              to bring it to your doorstep in the easiest possible way. SHOP IT
+              motto has always been to provide excellent quality and service.
             </p>
             <div class={aboutStyles.button}>
               <Link to="/">Shop</Link>
             </div>
           </div>
-        </div>
-        <div class={aboutStyles.image_section}>
-          <img src="image/img one.jpg" />
         </div>
       </div>
     </div>
